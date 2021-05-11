3a1f0fc2aa48a9ba0a5658e64dc6e29b6cb916d5
Jacek <jacek.poreda@sonarsource.com>
2021-05-11 13:37:21 +0200

    fixes

---
+++ script/check-quality-gate.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+ROOT="$(pwd)"
+source "$ROOT/common.sh"
+
+metadata_file="${INPUT_SCAN_METADATA_REPORT_FILE}"
+
+serverUrl="$(sed -n 's/serverUrl=\(.*\)/\1/p' $metadata_file)"
+ceTaskUrl="$(sed -n 's/ceTaskUrl=\(.*\)/\1/p' $metadata_file)"
+task="$(curl --silent --user $SONAR_TOKEN: $ceTaskUrl)"
+status="$(jq -r '.task.status' <<< "$task")"
+
+until [[ ${status} != "PENDING" && ${status} != "IN_PROGRESS" ]]; do
+    printf '.'
+    sleep 1
+    task="$(curl --silent --user $SONAR_TOKEN: $ceTaskUrl)"
+    status="$(jq -r '.task.status' <<< "$task")"
+done
+
+analysisId="$(jq -r '.task.analysisId' <<< "$task")"
+taskId="$(jq -r '.task.id' <<< "$task")"
+qualityGateUrl="${serverUrl}/api/qualitygates/project_status?analysisId=$analysisId"
+qualityGateStatus="$(curl --silent --user $SONAR_TOKEN: $qualityGateUrl | jq -r '.projectStatus.status')"
+
+if [[ ${qualityGateStatus} == "OK" ]];then
+   success "Quality Gate has PASSED."
+elif [[ ${qualityGateStatus} == "WARN" ]];then
+   warn "Warnings on Quality Gate."
+elif [[ ${qualityGateStatus} == "ERROR" ]];then
+   fail "Quality Gate has FAILED."
+else
+   fail "Quality Gate not set for the project. Please configure the Quality Gate in SonarQube or remove sonarqube-quality-gate action from the workflow."
+fi
+
